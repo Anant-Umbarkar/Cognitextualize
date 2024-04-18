ffa9abe27dd2120d957ff75d2da0d4141b3d61a9
kalashpatil33 <patilkalash33@gmail.com>
2024-04-18 11:39:24 +0530

    Minor changes in frontend and organized the Project Directory Structure.

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -1,28 +1,88 @@
-import React from 'react';
+// import React from "react";
 
 export default function Footer() {
   return (
     <div
       className="footer-container bg-dark text-light p-4"
-      style={{ textAlign: 'center' }}
+      style={{ textAlign: "center" }}
     >
       <div className="footer-content">
         <h1
           className="footer-title fs-4 fw-bold"
-          style={{ marginBottom: '10px' }}
+          style={{ marginBottom: "10px" }}
         >
-          Analyze Accurate. Improve Quality. with Cognitextualize
+          Cognitexualise: Unleashing Feedback Innovation through Question Paper
+          Analysis
         </h1>
         <div className="footer-contributors mt-4">
-          <h2 className="fs-5 fw-bold">Contributors</h2>
-          <ul className="list-unstyled">
-            <li>Kalash</li>
-            <li>Utkarsh</li>
-            <li>Virendra</li>
-            <li>Mitali</li>
-            <li>Aaditya</li>
-            <li>Vedant</li>
-          </ul>
+          <div>
+            <h2 className="fs-5 fw-bold">Contributors</h2>
+            <ul
+              className="list-unstyled"
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                gap: "10px",
+                flexWrap: "wrap",
+              }}
+            >
+              <li>
+                <a
+                  href="https://github.com/kalashpatil33"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Kalash Patil
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/UtkarshM-hub"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Utkarsh Mandape
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/virendra2003"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Virendra Patil
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/mitupatil18"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Mitali Patil
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/Aaditya123-exe"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Aaditya Patil
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/vedantv3"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Vedant Vedpathak
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
